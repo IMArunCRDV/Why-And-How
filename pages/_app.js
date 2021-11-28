ef2b9c16837adbf867452c4e993e00b40bd3d9b6
IMArunCRDV <arunindia1110cricket@gmail.com>
2021-11-28 12:32:08 +0530

    'deployed'

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import {Layout} from '../components'
+import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Head>
+    <title>Why and How</title>
+    <script data-ad-client="ca-pub-6618815321294218" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  </Head>
     <Layout>
   <Component {...pageProps} />
     </Layout>
